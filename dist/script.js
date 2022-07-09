53aebc7db08b09e010d32a790be25d355e2ea5bb
GlebasusBP <bgp13art@Gmail.com>
2022-07-09 20:28:15 +0300

    feat: timer will not wath negative valius when the incommind date has already passed

--- dist/script.js
+++ dist/script.js
@@ -43,13 +43,21 @@ window.addEventListener('DOMContentLoaded', () => {
   const deadLine = '2022-07-12';
 
   function getTimeRemaining(endTime) {
+    let days, hours, minutes, seconds;
+    const t = Date.parse(endTime) - Date.parse(new Date());
 
-    const t = Date.parse(endTime) - Date.parse(new Date()),
-          days = Math.floor(t / (1000 * 60 * 60 * 24)),
-          hours = Math.floor((t / (1000 * 60 * 60)) % 24),
-          minutes = Math.floor((t / (1000 * 60 )) % 60 ),
-          seconds = Math.floor((t / (1000)) % 60);
-     
+    if(t <= 0){
+      days = 0;
+      hours = 0;
+      minutes = 0;
+      seconds = 0;
+    } else {
+      days = Math.floor(t / (1000 * 60 * 60 * 24)),
+      hours = Math.floor((t / (1000 * 60 * 60)) % 24),
+      minutes = Math.floor((t / (1000 * 60 )) % 60 ),
+      seconds = Math.floor((t / (1000)) % 60);
+    }
+         
     return {
       total: t,
       days: days,
