25d344eeef0879f12fec147cba42a1046f558780
GlebasusBP <bgp13art@Gmail.com>
2022-07-09 20:15:03 +0300

    feat: task Timer is done

--- dist/script.js
+++ dist/script.js
@@ -38,4 +38,59 @@ window.addEventListener('DOMContentLoaded', () => {
     }
   })
 
+  // Timer
+
+  const deadLine = '2022-07-12';
+
+  function getTimeRemaining(endTime) {
+
+    const t = Date.parse(endTime) - Date.parse(new Date()),
+          days = Math.floor(t / (1000 * 60 * 60 * 24)),
+          hours = Math.floor((t / (1000 * 60 * 60)) % 24),
+          minutes = Math.floor((t / (1000 * 60 )) % 60 ),
+          seconds = Math.floor((t / (1000)) % 60);
+     
+    return {
+      total: t,
+      days: days,
+      hours: hours,
+      minutes: minutes,
+      seconds: seconds
+    };   
+  }
+
+
+  function setClock(selector, endTime){
+    const timer = document.querySelector(selector),
+          days = timer.querySelector('#days'),
+          hours = timer.querySelector('#hours'),
+          minutes = timer.querySelector('#minutes'),
+          seconds = timer.querySelector('#seconds'),
+          updateInterval = setInterval(updateClock, 1000);
+
+    updateClock();      
+
+    function updateClock(){
+      const time = getTimeRemaining(endTime);
+
+      function setZero(num){
+        if(num >= 0 && num < 10){
+          return `0${num}`;
+        }else{
+          return num;
+        }
+      }
+
+      days.innerHTML = setZero(time.days);
+      hours.innerHTML = setZero(time.hours);
+      minutes.innerHTML = setZero(time.minutes);
+      seconds.innerHTML = setZero(time.seconds);
+
+      if (time.total <= 0){
+        clearInterval(updateInterval);
+      }
+    }
+  }
+
+  setClock(".timer", deadLine);
 })
