e5c2c10bd82667a00dd6373f1ca4faf3755c558d
GlebasusBP <bgp13art@Gmail.com>
2022-07-11 14:42:56 +0300

    feat: task Modal Window is done

--- dist/main.js
+++ dist/main.js
@@ -1,35 +1,36 @@
 "use strict";
 
-window.addEventListener('DOMContentLoaded', () => {
+window.addEventListener("DOMContentLoaded", () => {
+// TABS
+  const tabContent = document.querySelectorAll(".tabcontent"),
+        parentTabs = document.querySelector(".tabheader__items"),
+        tabs = document.querySelectorAll(".tabheader__item");
 
-  const tabContent = document.querySelectorAll(".tabcontent");
-  const tabs = document.querySelectorAll(".tabheader__item");
-  const tabsParent = document.querySelector(".tabheader__items");
-
-  function hideTabContent (){
-    tabContent.forEach(elem => {
-      elem.classList.remove("show");
-      elem.classList.add("hide");
-    })
+  function hideTabContent(){
+    tabContent.forEach((e) => {
+      e.classList.add("hide");
+      e.classList.remove("show", "fade");
+    });
 
     tabs.forEach(elem => {
-      elem.classList.remove("tabheader__item_active");
+      elem.classList.remove("tabheader__item_active")
     })
   }
 
-  function showTabContent (i = 0){
-    tabContent[i].classList.add("show", "fade");
-    tabContent[i].classList.remove("hide");
-    tabs[i].classList.add("tabheader__item_active");
+  function showTabContent(item = 0){
+    tabContent[item].classList.remove("hide");
+    tabContent[item].classList.add("show", "fade");
+    tabs[item].classList.add("tabheader__item_active")
   }
 
   hideTabContent();
   showTabContent();
 
-  tabsParent.addEventListener('click', (event) => {
+  parentTabs.addEventListener("click", (event) => {
     const target = event.target;
+
     if(target && target.classList.contains("tabheader__item")){
-      tabs.forEach((elem, i) => {
+      tabs.forEach( (elem, i) => {
         if(target == elem){
           hideTabContent();
           showTabContent(i);
@@ -38,13 +39,18 @@ window.addEventListener('DOMContentLoaded', () => {
     }
   })
 
-  // Timer
+  // при клике на какой либо элемент родителя работает колбэк, в котором, если клик произошол по элементу с классом в условии,
+  // то запускется перебор элементов и сравнение их с элементом, по которому произошол клик. При true вызываются функции и 
+  // передаётся индекс элемента, который нужно показать на стр.
 
-  const deadLine = '2022-07-12';
 
-  function getTimeRemaining(endTime) {
+  //TIMER
+
+  const deadLine = '2022-07-15';
+
+  function getTimeRemaining(endTime){
     let days, hours, minutes, seconds;
-    const t = Date.parse(endTime) - Date.parse(new Date());
+    const t = Date.parse(endTime) - Date.parse(new Date());   
 
     if(t <= 0){
       days = 0;
@@ -52,39 +58,41 @@ window.addEventListener('DOMContentLoaded', () => {
       minutes = 0;
       seconds = 0;
     } else {
-      days = Math.floor(t / (1000 * 60 * 60 * 24)),
-      hours = Math.floor((t / (1000 * 60 * 60)) % 24),
-      minutes = Math.floor((t / (1000 * 60 )) % 60 ),
-      seconds = Math.floor((t / (1000)) % 60);
+      days = Math.floor(t / (1000 * 60 * 60 * 24));
+      hours = Math.floor((t / (1000 * 60 * 60) % 24));
+      minutes = Math.floor((t / (1000 * 60) % 60));
+      seconds = Math.floor((t / 1000) % 60);
     }
-         
+    
     return {
       total: t,
       days: days,
       hours: hours,
       minutes: minutes,
       seconds: seconds
-    };   
+    }
   }
-
+  
+  console.log(getTimeRemaining(deadLine));
+  
 
   function setClock(selector, endTime){
     const timer = document.querySelector(selector),
-          days = timer.querySelector('#days'),
-          hours = timer.querySelector('#hours'),
-          minutes = timer.querySelector('#minutes'),
-          seconds = timer.querySelector('#seconds'),
-          updateInterval = setInterval(updateClock, 1000);
+          days = timer.querySelector("#days"),
+          hours = timer.querySelector("#hours"),
+          minutes = timer.querySelector("#minutes"),
+          seconds = timer.querySelector("#seconds"),
+          updateInterval = setInterval(updateTimer, 1000);
 
-    updateClock();      
+    updateTimer();      
 
-    function updateClock(){
+    function updateTimer(){
       const time = getTimeRemaining(endTime);
 
       function setZero(num){
-        if(num >= 0 && num < 10){
+        if(num < 10){
           return `0${num}`;
-        }else{
+        } else {
           return num;
         }
       }
@@ -94,10 +102,10 @@ window.addEventListener('DOMContentLoaded', () => {
       minutes.innerHTML = setZero(time.minutes);
       seconds.innerHTML = setZero(time.seconds);
 
-      if (time.total <= 0){
+      if(time.total <= 0){
         clearInterval(updateInterval);
       }
-    }
+    }   
   }
 
   setClock(".timer", deadLine);
