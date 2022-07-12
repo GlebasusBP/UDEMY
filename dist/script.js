4d6828fc5089015531722016d2ba53a0acc7bab5
GlebasusBP <bgp13art@Gmail.com>
2022-07-12 23:00:59 +0300

    feat: task classes is done

--- dist/script.js
+++ dist/script.js
@@ -158,4 +158,69 @@ window.addEventListener('DOMContentLoaded', () => {
  }
 
  window.addEventListener('scroll', showModalByScroll);
+
+  // classes
+
+  class MenuCard {
+    constructor(src, alt, title, description, price, parentSelector){
+      this.src = src;
+      this.alt = alt;
+      this.title = title;
+      this.description = description;
+      this.price = price;
+      this.parentMenu = document.querySelector(parentSelector);
+      this.exchange = 30;
+      this.changePriceToUAH();
+
+    }
+
+    changePriceToUAH(){
+      this.price = this.price * this.exchange;
+    }
+
+    renderCard(){
+      const element = document.createElement('div');
+      element.innerHTML = `
+        <div class="menu__item">
+          <img src=${this.src} alt=${this.alt}>
+          <h3 class="menu__item-subtitle">${this.title}</h3>
+          <div class="menu__item-descr">${this.description}</div>
+          <div class="menu__item-divider"></div>
+          <div class="menu__item-price">
+              <div class="menu__item-cost">Цена:</div>
+              <div class="menu__item-total"><span>${this.price}</span> грн/день</div>
+          </div>
+        </div>
+        `;
+      
+      this.parentMenu.append(element);  
+    }
+  }
+
+  new MenuCard(
+    "img/tabs/vegy.jpg",
+    "vegy",
+    'Меню "Фитнес"',
+    'Меню "Фитнес" - это новый подход к приготовлению блюд: больше свежих овощей и фруктов. Продукт активных и здоровых людей. Это абсолютно новый продукт с оптимальной ценой и высоким качеством!',
+    9,
+    ".menu .container"
+  ).renderCard();
+
+  new MenuCard(
+    "img/tabs/elite.jpg",
+    "elite",
+    'Меню “Премиум”',
+    'В меню “Премиум” мы используем не только красивый дизайн упаковки, но и качественное исполнение блюд. Красная рыба, морепродукты, фрукты - ресторанное меню без похода в ресторан!',
+    15,
+    ".menu .container"
+  ).renderCard();
+
+  new MenuCard(
+    "img/tabs/post.jpg",
+    "post",
+    'Меню "Постное"',
+    'Меню “Постное” - это тщательный подбор ингредиентов: полное отсутствие продуктов животного происхождения, молоко из миндаля, овса, кокоса или гречки, правильное количество белков за счет тофу и импортных вегетарианских стейков.',
+    12,
+    ".menu .container"
+  ).renderCard();
 });
